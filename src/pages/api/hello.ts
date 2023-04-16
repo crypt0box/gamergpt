3fd4dfae1254299c2b979dd826e31bdbf3ecd4bf
cryptobox <pray,happy76@gmail.com>
2023-04-16 21:34:52 +0900

    feat: レビューを取得するところまで実装

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const params: { gameId: string } = req.body;
+  const { data } = await axios.get(
+    `https://store.steampowered.com/appreviews/${params.gameId}?json=1&language=japanese&day_range=365`
+  );
+  const review: string = data.reviews
+    .map((el: any) => el.review.replace(/\s+/g, ""))
+    .join();
+
+  res.status(200).json({ name: "John Doe" });
 }
